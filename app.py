71af383d3969788da0b5212bb8cbb65b645353a6
LeandroIber <leandroiber2016@gmail.com>
2026-05-05 14:34:46 -0300

    Adicionar Xlsx
    Added support for XLSX file uploads and flexible reading of CSV/XLSX for both dictionary and fact tables.

--- app.py
+++ app.py
@@ -11,16 +11,22 @@ st.markdown("Suba seus arquivos CSV para aplicar o dicionário global.")
 col1, col2 = st.columns(2)
 
 with col1:
-    dic_file = st.file_uploader("Suba o arquivo de dicionário (CSV)", type=['csv'])
+    # ALTERAÇÃO 1 adicionar opção xlsx: type agora aceita 'xlsx' além de 'csv' 
+    dic_file = st.file_uploader("Suba o arquivo de dicionário (CSV ou Excel)", type=['csv', 'xlsx'])
 
 with col2:
-    fato_files = st.file_uploader("Suba as tabelas Registro (fato) (Pode selecionar várias)", type=['csv'], accept_multiple_files=True)
+    # ALTERAÇÃO 2 adicionar opção xlsx: type agora aceita 'xlsx' além de 'csv' 
+    fato_files = st.file_uploader("Suba as tabelas Registro (fato) (Pode selecionar várias)", type=['csv', 'xlsx'], accept_multiple_files=True)
 
 # 2. Processamento
 if dic_file and fato_files:
-    # Carregar dicionário
-    dic_df = pd.read_csv(dic_file, sep=None, engine='python')
-    
+
+    # ALTERAÇÃO 3: leitura flexível do dicionário CSV ou XLSX
+    if dic_file.name.endswith('.csv'):
+        dic_df = pd.read_csv(dic_file, sep=None, engine='python')
+    else:
+        dic_df = pd.read_excel(dic_file)
+
     mapa_global = {}
     ignorar = ['antigo', 'novo', 'nan', 'dim_analista', 'dim_orgao', 'dim_cliente']
     
@@ -40,8 +46,12 @@ if dic_file and fato_files:
     colunas_alvo = ['dim_analista', 'dim_orgao', 'dim_identificacaoprojeto', 'dim_cliente']
 
     for file in fato_files:
-        # Ler arquivo fato
-        df = pd.read_csv(file, sep=None, engine='python')
+
+        # ALTERAÇÃO 4: leitura flexível das tabelas fato (registro)  CSV ou XLSX
+        if file.name.endswith('.csv'):
+            df = pd.read_csv(file, sep=None, engine='python')
+        else:
+            df = pd.read_excel(file)
         
         # Aplicar substituição
         for col in colunas_alvo:
